9a45949728eb84bca19e805f43a05918ad294c12
KarzLightyear <k.k.yamato@gmail.com>
2021-06-19 11:13:21 +0000

    [update]4-10課題問題bookページ左側まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @user = current_user
   end
 
   def show
@@ -27,7 +28,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :image)
   end
   
 end
