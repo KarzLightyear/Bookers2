3ffa969b39068dc9d4680fe0c9804e1b4c75a640
KarzLightyear <k.k.yamato@gmail.com>
2021-06-20 08:43:14 +0000

    [update]4-10課題問題bookのbooksまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.permit(:title, :body, :image)
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
+  def index
+    @user = current_user
+  end
+  
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page]).reverse_order
   end
 
   def edit
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
   
   private
   def user_params
-    params.require(:user).permit(:image)
+    params.require(:user).permit(:profile_image, :name, :introduction)
   end
   
 end
