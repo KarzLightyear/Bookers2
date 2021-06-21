2c568240bef922dbc264f88212db198cc9c2ab14
KarzLightyear <k.k.yamato@gmail.com>
2021-06-21 10:02:06 +0000

    [update]4-10課題問題ファイルの記述まで完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,22 +13,35 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
+    @book_new = Book.new
   end
 
   def show
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    @user = current_user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to bookss_path
+    redirect_to books_path
   end
-  
+
   private
 
   def book_params
-    params.permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,28 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
     @user = current_user
+    @book_new = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
+    @book_new = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
     redirect_to user_path(user.id)
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:profile_image, :name, :introduction)
   end
-  
+
 end
