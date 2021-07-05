487f574af362608323e792b440f411986af64f11
KarzLightyear <k.k.yamato@gmail.com>
2021-07-05 08:53:32 +0000

    [finish]応用課題4

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-  end  
-  resources :users, only: [:index, :show, :edit, :update]
+  end
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
 end
